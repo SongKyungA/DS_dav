32a47b143727b40e407dc6974ddaa35b64492675
AdolphSeoul <kimbw0909@naver.com>
2023-12-02 18:32:35 +0900

    graph modify

--- front/krecipe.js
+++ front/krecipe.js
@@ -1,15 +1,15 @@
 // krecipe.js
 
-// JSON 파일을 로드합니다.
+// JSON 파일을 로드
 d3.json("krecipe_graph.json").then(function(graph) {
-    // 그래프의 너비와 높이를 설정합니다.
+    // 그래프의 너비와 높이를 설정
     const width = window.innerWidth;
     const height = window.innerHeight;
-    const nodeRadius = 20; // 노드의 크기를 20으로 증가
+    const nodeRadius = 20;
 
     // SVG 요소를 생성하고 크기를 설정합니다.
     const svg = d3.select("body").append("svg")
-        .attr("viewBox", [0, 0, width, height]) // viewBox 사용
+        .attr("viewBox", [0, 0, width, height])
         .style("width", "100%")
         .style("height", "100vh");
 
@@ -42,24 +42,24 @@ d3.json("krecipe_graph.json").then(function(graph) {
 
     // SVG에 줌 핸들러를 적용
     svg.call(zoomHandler);
-    // 요리명 노드와 재료 노드를 구분합니다.
+    
     const recipeNode = svg.append("g")
         .attr("class", "recipeNodes")
         .selectAll("circle")
-        .data(graph.nodes.filter(node => node.group === 1)) // 요리명 노드만 선택
+        .data(graph.nodes.filter(node => node.group === 1))
         .enter()
         .append("circle")
         .attr("class", "recipeNode")
-    // 설정된 노드 속성 및 이벤트 핸들러...
+    
 
     const ingredientNode = svg.append("g")
         .attr("class", "ingredientNodes")
         .selectAll("circle")
-        .data(graph.nodes.filter(node => node.group !== 1)) // 재료 노드만 선택
+        .data(graph.nodes.filter(node => node.group !== 1))
         .enter()
         .append("circle")
         .attr("class", "ingredientNode")
-    // 링크를 그립니다.
+    // 링크
     const link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
@@ -67,7 +67,7 @@ d3.json("krecipe_graph.json").then(function(graph) {
         .enter().append("line")
         .attr("class", "link");
 
-    // 노드를 그립니다.
+    // 노드
     const node = svg.append("g")
         .attr("class", "nodes")
         .selectAll("circle")
@@ -75,31 +75,43 @@ d3.json("krecipe_graph.json").then(function(graph) {
         .enter().append("circle")
         .attr("class", "node")
         .attr("r", nodeRadius)
-        .style("fill", function(d) {
-            // 그룹 값에 따라 다른 색상 적용
-            return d.group === 1 ? "#ff5733" : "#6699cc";
-        })
+        .style("fill", function(d) {return d.group === 1 ? "#ff5733" : "#6699cc";})
         .on("click", function(event, d) {
             if (d.url) {
                 window.open(d.url, "_blank");
             }
+            // 선택한 노드의 ID를 업데이트하고 연결된 노드를 필터링
+            selectedNodeId = d.id;
+
+            // 연결된 노드만 표시
+            node.style("display", function(nodeData) {
+                return isConnected(nodeData, d) ? "block" : "none";
+            });
+
+            label.style("display", function(nodeData) {
+                return isConnected(nodeData, d) ? "block" : "none";
+            });
+
+            link.style("display", function(linkData) {
+                return linkData.source.id === selectedNodeId || linkData.target.id === selectedNodeId ? "block" : "none";
+            });
         })
-        // 툴팁 스타일 변경
+        // 툴팁 스타일
         .on("mouseover", function(event, d) {
             tooltip.transition()
                 .duration(200)
                 .style("opacity", .9)
-                .style("background", "rgba(0, 0, 0, 0.8)") // 호버 시 배경색 변경
-                .style("color", "#fff"); // 호버 시 글자 색상 변경
+                .style("background", "rgba(0, 0, 0, 0.8)")
+                .style("color", "#fff");
             
-            // "target" 속성을 사용하여 툴팁에 연결된 정보 표시
+            
             const linkedTarget = graph.links.find(link => link.source === d.id)?.target;
             if (linkedTarget) {
                 tooltip.html(d.id + "<br>" + linkedTarget)
                     .style("left", (event.pageX) + "px")
                     .style("top", (event.pageY - 28) + "px");
             } else {
-                tooltip.html(d.id + "<br>No target information") // 연결된 정보가 없는 경우 메시지 표시
+                tooltip.html(d.id + "<br>No target information")
                     .style("left", (event.pageX) + "px")
                     .style("top", (event.pageY - 28) + "px");
             }
@@ -108,11 +120,11 @@ d3.json("krecipe_graph.json").then(function(graph) {
             tooltip.transition()
                 .duration(500)
                 .style("opacity", 0)
-                .style("background", "rgba(0, 0, 0, 0.8)") // 마우스 아웃 시 배경색 복원
-                .style("color", "#fff"); // 마우스 아웃 시 글자 색상 복원
+                .style("background", "rgba(0, 0, 0, 0.8)")
+                .style("color", "#fff");
         });
 
-    // 레이블을 그립니다.
+    // 레이블
     const label = svg.append("g")
         .attr("class", "labels")
         .selectAll("text")
@@ -120,9 +132,12 @@ d3.json("krecipe_graph.json").then(function(graph) {
         .enter().append("text")
         .text(function(d) { return d.id; })
         .attr("x", 8)
-        .attr("y", ".31em");
+        .attr("y", ".31em")
+        .style("font", "12px 'BMJUA', sans-serif")       
     
-    
+    const collide = d3.forceCollide(nodeRadius + 5).iterations(4); // 충돌 감지 반복 횟수 조절
+    simulation.force("collide", collide);    
+
     // 시뮬레이션 갱신 함수
     function ticked() {
         link
@@ -132,6 +147,8 @@ d3.json("krecipe_graph.json").then(function(graph) {
             .attr("y2", function(d) { return d.target.y; });
 
         node
+            .transition()
+            .duration(1000) // 애니메이션 지속 시간
             .attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
 
@@ -142,4 +159,23 @@ d3.json("krecipe_graph.json").then(function(graph) {
 
     simulation
         .on("tick", ticked);
+    
+    // 노드 간의 연결 여부를 확인하는 함수
+    function isConnected(node1, node2) {
+        return graph.links.some(link => (link.source === node1 && link.target === node2) || (link.source === node2 && link.target === node1));
+    }
+    // 선택 원복 버튼을 가져옴
+    const restoreButton = document.getElementById("restoreButton");
+
+    // 선택 원복 버튼 클릭 이벤트 핸들러
+    restoreButton.addEventListener("click", function() {
+        // 선택한 노드와 연결된 노드를 다시 표시
+        node.style("display", "block");
+        label.style("display", "block");
+        link.style("display", "block");
+        
+        // 선택 취소
+        selectedNodeId = null;
+    });
+    
 });
